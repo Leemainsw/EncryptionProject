e28690a5acae3efc3cac30e51e53ac48dffd58de
Leemainsw <s2018s15@e-mirim.hs.kr>
2020-08-07 22:58:26 +0900

    암호화

--- Project/app.js
+++ Project/app.js
@@ -3,6 +3,8 @@ const bodyparser = require('body-parser');
 const oracledb = require('oracledb');
 const app = express();
 const port = 3000;
+var conn;
+var currentUser;
 
 app.set('views', './views')
 app.set('view engine', 'pug')
@@ -11,6 +13,21 @@ app.locals.pretty = true;
 app.use(bodyparser.urlencoded({
     exrended: false
 }))
+
+//오라클 접속
+oracledb.getConnection({
+    user:"SSNCTEST",
+    password:"SSNC1234",
+    connectString:"localhost/XE" //oracle설치할때 지정한 이름(파일명으로 확인가능)
+},function(err,con){
+    if(err){
+        console.log("접속이 실패했습니다.", err);
+        return;
+    }
+    console.log('접속에 성공하였습니다')
+    conn = con;
+
+});
 
 function Encryption(arr) {
     var _arr = arr.split("");
@@ -24,6 +41,16 @@ function Encryption(arr) {
         if( i%2 != 0 ) result1[i] = null;
         result2[i] = (String.fromCharCode(result1[i]));
     }
+    conn.execute(`INSERT INTO ENCRYPTION (STRING, ENCRYPTION, ID) VALUES('${arr}' , '${result2}' , ${currentUser})`,function(err,result){
+        console.log(`INSERT INTO 'SSNCTEST'.'ENCRYPTION' (STRING, ENCRYPTION, ID) VALUES('${arr}' , '${result2}' , ${currentUser})`);
+        if(err){
+            console.log("등록중 에러가 발생했어요!!", err);
+        }else{
+            console.log("result : ", result);
+        }
+    });
+    // INSERT INTO "SSNCTEST"."ENCRYPTION" (STRING, ENCRYPTION, ID) VALUES ('I''m bussy', '䘩,,촭,,죪,,,,', '1')
+
     console.log(result2);
     return result2;
 }
@@ -59,19 +86,6 @@ app.post('/Login', (req, res)=>{
     console.log('password = ' + password);
 
     // 아이디와 비밀번호가 일치하면 암호화 페이지로 넘김
-    oracledb.getConnection({
-        user: 'SSNCTEST',
-        password: 'SSNC1234',
-        host: 'localhost', 
-        database: 'xe'
-    }, function (err, connection) {
-        if(err){
-            console.log('접속 실패', err);
-            return;
-        }
-        console.log('접속 성공');
-        conn = connection;
-
         conn.execute("select * from users", {}, {outFormat:oracledb.OBJECT}, function (err, result) {  
             // Json 형태로 넘어오도록 설정
             if(err) throw err;  
@@ -92,7 +106,7 @@ app.post('/Login', (req, res)=>{
 
                         console.log('로그인 성공!');
                         res.send(`<script type="text/javascript">alert("로그인 성공!");location.href="/Encryption";</script>`);
-                        return;
+                        currentUser = arr[i].ID;
                     }
                     else
                     {
@@ -105,8 +119,6 @@ app.post('/Login', (req, res)=>{
             console.log('아이디가 존재하지 않습니다!');
             res.send(`<script type="text/javascript">alert("아이디가 존재하지 않습니다");location.href="/Login";</script>`);
         });
-    });
-    
 })
 
 
